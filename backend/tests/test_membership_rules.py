a41771d72537eafa40585608664df9da63e93261
Paul <phaul.thompson@gmail.com>
2022-01-27 16:52:32 -0800

    Test removal, leaving and deactivation rules

--- backend/tests/test_membership_rules.py
+++ backend/tests/test_membership_rules.py
@@ -126,3 +126,98 @@ def test_assignable_roles_lists_exactly_the_allowed_moves():
 # ---------- Removal, leaving and inviting ----------
 
 
+def test_removal_matrix():
+    for actor, target in product(ROLES, ROLES):
+        denial = removal_denial(actor_role=actor, target_role=target, is_self=False)
+        allowed = ROLE_RANK[actor] >= ROLE_RANK["admin"] and (target != "owner" or actor == "owner")
+        assert (denial is None) == allowed, (actor, target)
+
+
+def test_nobody_removes_themselves():
+    for role in ("admin", "owner"):
+        denial = removal_denial(actor_role=role, target_role=role, is_self=True)
+        assert denial.status == 409 and "Leave" in denial.reason
+    assert removal_denial(actor_role="learner", target_role="learner", is_self=True).status == 403
+
+
+def test_last_owner_cannot_leave():
+    assert leave_denial(role="owner", owner_count=1).status == 409
+    assert leave_denial(role="owner", owner_count=2) is None
+    for role in ("learner", "instructor", "admin"):
+        assert leave_denial(role=role, owner_count=1) is None
+
+
+def test_invite_roles():
+    assert invite_role_denial(actor_role="learner", role="learner").status == 403
+    assert invite_role_denial(actor_role="instructor", role="learner").status == 403
+    assert invite_role_denial(actor_role="admin", role="owner").status == 403
+    assert invite_role_denial(actor_role="admin", role="banana").status == 422
+    for role in ("learner", "instructor", "admin"):
+        assert invite_role_denial(actor_role="admin", role=role) is None
+    assert invite_role_denial(actor_role="owner", role="owner") is None
+
+
+# ---------- Deactivation ----------
+
+
+def test_deactivation_is_blocked_only_by_shared_workspaces_without_another_owner():
+    seats = [
+        WorkspaceSeat("Solo space", "owner", owner_count=1, member_count=1),
+        WorkspaceSeat("Zoo club", "owner", owner_count=1, member_count=4),
+        WorkspaceSeat("Co-owned", "owner", owner_count=2, member_count=6),
+        WorkspaceSeat("Art guild", "owner", owner_count=1, member_count=2),
+        WorkspaceSeat("Book club", "admin", owner_count=1, member_count=9),
+    ]
+    assert deactivation_blockers(seats) == ["Art guild", "Zoo club"]
+    assert deactivation_blockers([]) == []
+
+
+# ---------- Invitation state and confirmation ----------
+
+
+def test_invitation_state_marks_past_pending_invitations_expired():
+    now = datetime(2022, 3, 14, 9)
+    assert invitation_state("pending", now + timedelta(seconds=1), now) == "pending"
+    assert invitation_state("pending", now, now) == "expired"
+    assert invitation_state("pending", now - timedelta(days=3), now) == "expired"
+    assert invitation_state("accepted", now - timedelta(days=3), now) == "accepted"
+    assert invitation_state("revoked", now + timedelta(days=3), now) == "revoked"
+
+
+def test_confirmation_requires_the_exact_name():
+    assert confirmation_matches("  Northwind Data Academy ", "Northwind Data Academy")
+    assert not confirmation_matches("northwind data academy", "Northwind Data Academy")
+    assert not confirmation_matches("Northwind", "Northwind Data Academy")
+    assert not confirmation_matches("", "Northwind Data Academy")
+
+
+# ---------- Email list parsing ----------
+
+
+def test_parse_email_list_splits_normalises_and_dedupes():
+    parsed = parse_email_list(["Ada@Example.com, bob@example.org;carol@example.net\n\n ada@example.com", "dan@x.io"])
+    assert parsed.valid == ["ada@example.com", "bob@example.org", "carol@example.net", "dan@x.io"]
+    assert parsed.duplicates == ["ada@example.com"]
+    assert parsed.invalid == []
+
+
+def test_parse_email_list_understands_display_names():
+    parsed = parse_email_list(['Ada Lovelace <ada@example.com>, "Bob" <BOB@example.org>'])
+    assert parsed.valid == ["ada@example.com", "bob@example.org"]
+    assert parsed.invalid == []
+
+
+def test_parse_email_list_reports_invalid_entries_as_typed():
+    parsed = parse_email_list(["not-an-email, x@y, ok@fine.dev, @nope.com"])
+    assert parsed.valid == ["ok@fine.dev"]
+    assert parsed.invalid == ["not-an-email", "x@y", "@nope.com"]
+
+
+def test_parse_email_list_rejects_overlong_addresses():
+    long_email = "a" * 250 + "@x.io"
+    assert parse_email_list([long_email]).invalid == [long_email]
+
+
+def test_split_addresses_ignores_blank_input():
+    assert split_addresses("  ,, ;\n\t ") == []
+    assert parse_email_list(["", "   "]).valid == []
