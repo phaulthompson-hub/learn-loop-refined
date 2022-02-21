301bb71f6e527c3a982f1ddb5347342bdfa4d9bf
Paul <phaul.thompson@gmail.com>
2022-02-21 10:22:32 -0800

    Add workspace create, read, update and delete endpoints

--- backend/app/routers/workspaces.py
+++ backend/app/routers/workspaces.py
@@ -247,3 +247,124 @@ def sync_primary_owner(db: Session, workspace: Workspace) -> None:
         workspace.owner_id = owners[0].user_id
 
 
+def purge_rows(db: Session, table: Table, where: ColumnElement) -> None:
+    """Delete rows and everything that references them, following each foreign key's `ondelete` rule.
+
+    SQLite only enforces ON DELETE clauses when `PRAGMA foreign_keys` is on, so deleting a workspace
+    walks the schema itself: CASCADE children are purged recursively and SET NULL columns are cleared.
+    """
+    primary = table.c.get("id")
+    if primary is not None:
+        ids = list(db.scalars(select(primary).where(where)))
+        if not ids:
+            return
+        for child in Base.metadata.sorted_tables:
+            for fk in child.foreign_keys:
+                if fk.column is not primary or child is table:
+                    continue
+                if fk.ondelete == "CASCADE":
+                    purge_rows(db, child, fk.parent.in_(ids))
+                elif fk.ondelete == "SET NULL":
+                    db.execute(update(child).where(fk.parent.in_(ids)).values({fk.parent.name: None}))
+    db.execute(delete(table).where(where))
+
+
+def invite_notification(db: Session, invitation: Invitation, workspace: Workspace, actor: User) -> None:
+    invitee = find_user_by_email(db, invitation.email)
+    if invitee is None:
+        return
+    notify(
+        db,
+        user_id=invitee.id,
+        actor_id=actor.id,
+        kind="invite",
+        title=f"{actor.name} invited you to {workspace.name}",
+        body=invitation.message or f"Join as {article(invitation.role)} {invitation.role}.",
+        link=f"/invite/{invitation.token}",
+    )
+
+
+def forget_workspace(db: Session, workspace_id: int, user_ids: Iterable[int]) -> None:
+    """Stop remembering a workspace as someone's last opened one (they lost access to it)."""
+    ids = list(user_ids)
+    if ids:
+        db.execute(
+            update(User).where(User.id.in_(ids), User.last_workspace_id == workspace_id).values(last_workspace_id=None)
+        )
+
+
+# ---------- Workspaces ----------
+
+
+@router.post("/api/workspaces", response_model=WorkspaceOut, status_code=201)
+def add_workspace(data: WorkspaceCreate, user: User = Depends(current_user), db: Session = Depends(get_db)):
+    """Create a workspace owned by the caller and switch to it."""
+    workspace = create_workspace(db, owner=user, name=data.name, description=data.description, color=data.color)
+    user.last_workspace_id = workspace.id
+    record(
+        db,
+        workspace_id=workspace.id,
+        actor_id=user.id,
+        verb="workspace.created",
+        object_type="workspace",
+        object_id=workspace.id,
+        summary=f"created the workspace {workspace.name}",
+        link="/settings/workspace",
+    )
+    db.commit()
+    db.refresh(workspace)
+    return workspace_payload(db, Access(user=user, workspace=workspace, membership=workspace.memberships[0]))
+
+
+@router.get("/api/workspaces/{workspace_id}", response_model=WorkspaceOut)
+def get_workspace(access: Access = Depends(workspace_access), db: Session = Depends(get_db)):
+    return workspace_payload(db, access)
+
+
+@router.patch("/api/workspaces/{workspace_id}", response_model=WorkspaceOut)
+def update_workspace(data: WorkspaceUpdate, access: Access = Depends(workspace_access), db: Session = Depends(get_db)):
+    access.require("admin")
+    workspace = access.workspace
+    changes = {k: v for k, v in data.dict(exclude_none=True).items() if getattr(workspace, k) != v}
+    if changes:
+        renamed = "name" in changes
+        for key, value in changes.items():
+            setattr(workspace, key, value)
+        record(
+            db,
+            workspace_id=workspace.id,
+            actor_id=access.user.id,
+            verb="workspace.updated",
+            object_type="workspace",
+            object_id=workspace.id,
+            summary=f"renamed the workspace to {workspace.name}" if renamed else "updated the workspace settings",
+            link="/settings/workspace",
+        )
+        db.commit()
+    return workspace_payload(db, access)
+
+
+@router.delete("/api/workspaces/{workspace_id}", status_code=204)
+def delete_workspace(data: WorkspaceDelete, access: Access = Depends(workspace_access), db: Session = Depends(get_db)):
+    """Permanently delete a workspace and all of its content. Owners only; the name must be typed to confirm."""
+    access.require("owner")
+    workspace = access.workspace
+    if not confirmation_matches(data.confirm_name, workspace.name):
+        raise HTTPException(422, "Type the workspace name exactly to confirm")
+    name, workspace_id = workspace.name, workspace.id
+    member_ids = [m.user_id for m in workspace.memberships]
+    purge_rows(db, Workspace.__table__, Workspace.id == workspace_id)
+    forget_workspace(db, workspace_id, member_ids)
+    for user_id in member_ids:
+        notify(
+            db,
+            user_id=user_id,
+            actor_id=access.user.id,
+            kind="system",
+            title=f"{access.user.name} deleted the workspace {name}",
+            body="Its courses, notes, boards and calendars are no longer available.",
+        )
+    db.commit()
+    return Response(status_code=204)
+
+
