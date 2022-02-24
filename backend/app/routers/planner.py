99e35bad7a46bd3673f0aba9f6d09df963d08b38
Paul <phaul.thompson@gmail.com>
2022-02-24 14:15:38 -0800

    Add planner router helpers: visibility, payloads and filters

---
+++ backend/app/routers/planner.py
@@ -0,0 +1,137 @@
+"""Calendar events (with recurrence) for the planner.
+
+Visibility: a member sees their own events plus every shared event of the workspace. Only the
+owner edits an event; admins may also edit or delete shared ones. Shared exams and live sessions
+announce themselves to the workspace, so creating them needs the instructor role.
+"""
+
+from datetime import datetime, timedelta
+
+from fastapi import APIRouter, Depends, HTTPException, Query, Response
+from pydantic import ValidationError
+from sqlalchemy import or_, select
+from sqlalchemy.orm import Session
+
+from .. import clock
+from ..database import get_db
+from ..deps import Access, access_for, current_user, get_or_404, workspace_access
+from ..models import Course, Event, Membership, User
+from ..schemas.common import error_loc
+from ..schemas.planner import (
+    AgendaOut,
+    ConflictOut,
+    EventIn,
+    EventOut,
+    EventPatch,
+    EventSaved,
+    OccurrencePage,
+    naive_utc,
+)
+from ..services.events import notify, record
+from ..services.ics import IcsEvent, build_calendar, event_uid
+from ..services.recurrence import find_conflicts, occurrences
+
+router = APIRouter(tags=["planner"])
+
+MAX_RANGE = timedelta(days=124)  # a 6-week month grid with room to spare
+ANNOUNCED_KINDS = ("exam", "live")
+KIND_LABELS = {
+    "study": "study session",
+    "review": "review block",
+    "exam": "exam",
+    "deadline": "deadline",
+    "live": "live session",
+}
+
+
+# ---------- Helpers ----------
+
+
+def validation_detail(exc: ValidationError) -> list[dict]:
+    """JSON-safe version of a pydantic error list, shaped like FastAPI's own 422 responses."""
+    return [{"loc": error_loc(error["loc"]), "msg": error["msg"]} for error in exc.errors()]
+
+
+def can_edit(event: Event, access: Access) -> bool:
+    return event.user_id == access.user.id or (event.shared and access.can("admin"))
+
+
+def visible_events(db: Session, access: Access) -> list[Event]:
+    query = (
+        select(Event)
+        .where(Event.workspace_id == access.workspace.id, or_(Event.user_id == access.user.id, Event.shared.is_(True)))
+        .order_by(Event.starts_at, Event.id)
+    )
+    return list(db.scalars(query))
+
+
+def event_payload(event: Event, access: Access, people: dict[int, User], courses: dict[int, Course]) -> dict:
+    owner = people.get(event.user_id)
+    course = courses.get(event.course_id) if event.course_id else None
+    return {
+        "id": event.id,
+        "workspace_id": event.workspace_id,
+        "title": event.title,
+        "kind": event.kind,
+        "course": {"id": course.id, "title": course.title, "color": course.color} if course else None,
+        "owner": {"id": owner.id, "name": owner.name, "avatar_color": owner.avatar_color}
+        if owner
+        else {"id": event.user_id, "name": "Former member", "avatar_color": "#9aa59f"},
+        "starts_at": event.starts_at,
+        "ends_at": event.ends_at,
+        "all_day": event.all_day,
+        "location": event.location,
+        "notes": event.notes,
+        "recurrence": event.recurrence,
+        "recurrence_until": event.recurrence_until,
+        "shared": event.shared,
+        "created_at": event.created_at,
+        "can_edit": can_edit(event, access),
+    }
+
+
+class Payloads:
+    """Builds event payloads with owners and courses loaded once per request."""
+
+    def __init__(self, db: Session, access: Access, events: list[Event]):
+        self.access = access
+        user_ids = {e.user_id for e in events}
+        course_ids = {e.course_id for e in events if e.course_id}
+        self.people = {u.id: u for u in db.scalars(select(User).where(User.id.in_(user_ids)))}
+        self.courses = {c.id: c for c in db.scalars(select(Course).where(Course.id.in_(course_ids)))}
+        self.cache: dict[int, dict] = {}
+
+    def event(self, event: Event) -> dict:
+        if event.id not in self.cache:
+            self.cache[event.id] = event_payload(event, self.access, self.people, self.courses)
+        return self.cache[event.id]
+
+
+def parse_kinds(kind: str | None) -> set[str] | None:
+    if not kind:
+        return None
+    kinds = {k.strip() for k in kind.split(",") if k.strip()}
+    unknown = kinds - set(KIND_LABELS)
+    if unknown:
+        raise HTTPException(422, f"Unknown event kind: {', '.join(sorted(unknown))}")
+    return kinds
+
+
+def filter_events(
+    events: list[Event],
+    user_id: int,
+    kinds: set[str] | None,
+    course_id: int | None,
+    mine: bool | None,
+    shared: bool | None,
+) -> list[Event]:
+    return [
+        e
+        for e in events
+        if (kinds is None or e.kind in kinds)
+        and (course_id is None or e.course_id == course_id)
+        and (mine is None or (e.user_id == user_id) == mine)
+        and (shared is None or e.shared == shared)
+    ]
+
+
