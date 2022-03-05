0c06b026dd43b3b420018a4d3db2d37f41c1a7d1
Paul <phaul.thompson@gmail.com>
2022-03-04 17:01:16 -0800

    Add workspace analytics endpoint

--- backend/app/routers/analytics.py
+++ backend/app/routers/analytics.py
@@ -134,3 +134,178 @@ def concept_rows(
     return rows
 
 
+def course_rows(
+    courses: Sequence[Course],
+    concepts: dict[int, list[LearnerConcept]],
+    attempts: Sequence[Attempt],
+    reviews: Sequence[ReviewLog],
+    logs: Sequence[StudyLog],
+    period: stats.Period,
+) -> list[dict]:
+    start = stats.mastery_snapshot((c.id for views in concepts.values() for c in views), attempts, period.start_at)
+    rows = []
+    for course in courses:
+        views = concepts[course.id]
+        mine = [a for a in attempts if a.course_id == course.id and period.contains(a.created_at)]
+        mastery = average_mastery(views)
+        rows.append(
+            {
+                "id": course.id,
+                "title": course.title,
+                "color": course.color,
+                "concepts": len(views),
+                "mastered_concepts": stats.mastered_count(c.mastery for c in views),
+                "mastery": mastery,
+                "mastery_change": round(mastery - stats.average(start[c.id] for c in views), 1) if views else 0.0,
+                "attempts": len(mine),
+                "accuracy": stats.accuracy(sum(a.correct for a in mine), len(mine)),
+                "reviews": sum(1 for r in reviews if r.course_id == course.id),
+                "minutes": sum(log.minutes for log in logs if log.course_id == course.id),
+            }
+        )
+    return rows
+
+
+def time_breakdown(logs: Sequence[StudyLog], courses: Sequence[Course]) -> dict:
+    by_id = {c.id: c for c in courses}
+    per_course = stats.minutes_by(logs, lambda log: log.course_id, lambda log: log.minutes)
+    per_activity = stats.minutes_by(logs, lambda log: log.activity, lambda log: log.minutes)
+    return {
+        "total": sum(log.minutes for log in logs),
+        "by_course": [
+            {
+                "key": str(course_id) if course_id is not None else "none",
+                "label": by_id[course_id].title if course_id in by_id else UNASSIGNED,
+                "minutes": minutes,
+                "color": by_id[course_id].color if course_id in by_id else None,
+            }
+            for course_id, minutes in per_course.items()
+        ],
+        "by_activity": [
+            {"key": activity, "label": ACTIVITY_LABELS.get(activity, activity.title()), "minutes": minutes}
+            for activity, minutes in per_activity.items()
+        ],
+    }
+
+
+def leaderboard(db: Session, access: Access, course_ids: Sequence[int], period: stats.Period) -> list[dict]:
+    """Answers and accuracy per member this period. Learners only see first names; staff see full names."""
+    counts = {
+        user_id: (answers, correct or 0)
+        for user_id, answers, correct in db.execute(
+            select(Attempt.user_id, func.count(), func.sum(cast(Attempt.correct, Integer)))
+            .where(
+                Attempt.course_id.in_(course_ids),
+                Attempt.created_at >= period.start_at,
+                Attempt.created_at < period.end_at,
+            )
+            .group_by(Attempt.user_id)
+        )
+    }
+    members = db.scalars(
+        select(User)
+        .join(Membership, Membership.user_id == User.id)
+        .where(Membership.workspace_id == access.workspace.id)
+    )
+    staff = access.can("instructor")
+    rows = [
+        {
+            "user_id": member.id,
+            "name": member.name if staff or member.id == access.user.id else stats.first_name(member.name),
+            "avatar_color": member.avatar_color,
+            "answers": counts.get(member.id, (0, 0))[0],
+            "correct": counts.get(member.id, (0, 0))[1],
+        }
+        for member in members
+        if member.id in counts or member.id == access.user.id
+    ]
+    return stats.rank_leaderboard(rows, access.user.id)
+
+
+@router.get("/api/workspaces/{workspace_id}/analytics", response_model=AnalyticsOut)
+def get_analytics(
+    access: Access = Depends(workspace_access),
+    db: Session = Depends(get_db),
+    days: int = Query(30),
+    course_id: int | None = Query(None, ge=1),
+):
+    check_days(days)
+    courses = scope_courses(db, access, course_id)
+    user_id = access.user.id
+    period = stats.period_ending(clock.today(), days)
+    previous = period.previous()
+    course_ids = [c.id for c in courses]
+
+    attempts = user_attempts(db, user_id, course_ids)
+    reviews = reviews_since(db, user_id, course_ids, previous.start_at)
+    logs = study_logs_since(db, user_id, course_ids, previous.start_at, include_unassigned=course_id is None)
+    concepts = {c.id: learner_concepts(db, c, user_id) for c in courses}
+    concept_ids = [c.id for views in concepts.values() for c in views]
+
+    def split(rows, moment):
+        return stats.in_period(rows, period, moment), stats.in_period(rows, previous, moment)
+
+    now_attempts, before_attempts = split(attempts, lambda a: a.created_at)
+    now_reviews, before_reviews = split(reviews, lambda r: r.reviewed_at)
+    now_logs, before_logs = split(logs, lambda log: log.logged_at)
+    start = stats.mastery_snapshot(concept_ids, attempts, period.start_at)
+    end = stats.mastery_snapshot(concept_ids, attempts, period.end_at)
+    timeline = stats.mastery_timeline(concept_ids, attempts, period.dates())
+    now_retention = stats.retention(r.grade for r in now_reviews)
+    before_retention = stats.retention(r.grade for r in before_reviews)
+
+    def correct(rows):
+        return sum(a.correct for a in rows)
+
+    kpis = {
+        "answers": stats.compare(len(now_attempts), len(before_attempts)),
+        "accuracy": stats.compare(
+            stats.accuracy(correct(now_attempts), len(now_attempts)),
+            stats.accuracy(correct(before_attempts), len(before_attempts)),
+        ),
+        "reviews": stats.compare(len(now_reviews), len(before_reviews)),
+        "retention": stats.compare(now_retention or 0.0, before_retention or 0.0),
+        "minutes": stats.compare(sum(x.minutes for x in now_logs), sum(x.minutes for x in before_logs)),
+        "mastery": stats.compare(stats.average(end.values()), stats.average(start.values())),
+        "mastered_concepts": stats.compare(stats.mastered_count(end.values()), stats.mastered_count(start.values())),
+        "active_days": stats.compare(
+            stats.active_days(period, now_attempts, now_reviews, now_logs),
+            stats.active_days(previous, before_attempts, before_reviews, before_logs),
+        ),
+    }
+    daily = stats.daily_series(period.dates(), now_attempts, now_reviews, now_logs)
+    for row, mastery in zip(daily, timeline, strict=True):
+        row["mastery"] = mastery
+
+    rows = concept_rows(courses, concepts, attempts, period)
+    grades = [r.grade for r in now_reviews]
+    due_dates = card_due_dates(db, user_id, course_ids)
+    board_courses = course_ids if course_id is not None else [c.id for c in workspace_courses(db, access)]
+    return {
+        "range": {
+            "days": days,
+            "start": period.start,
+            "end": period.end,
+            "previous_start": previous.start,
+            "previous_end": previous.end,
+            "course_id": course_id,
+        },
+        "courses_in_scope": [brief(c) for c in courses],
+        "kpis": kpis,
+        "daily": daily,
+        "courses": course_rows(courses, concepts, attempts, now_reviews, now_logs, period),
+        "concepts": rows,
+        "weakest": stats.weakest(rows),
+        "flashcards": {
+            "reviews": len(grades),
+            "retention": now_retention,
+            "grades": stats.grade_counts(grades),
+            "due_now": sum(1 for due in due_dates if due < end_of_today()),
+            "forecast": stats.forecast(due_dates, period.end),
+        },
+        "time": time_breakdown(now_logs, courses),
+        "leaderboard": leaderboard(db, access, board_courses, period),
+        "can_view_learners": access.can("instructor"),
+    }
+
+
