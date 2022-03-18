944392c26610f99199e8122d140645564a6ad3d0
Paul <phaul.thompson@gmail.com>
2022-03-18 12:25:38 -0700

    Test workspace deletion

--- backend/tests/test_workspaces_api.py
+++ backend/tests/test_workspaces_api.py
@@ -121,3 +121,59 @@ def test_update_permissions_and_validation(client, sam, alex, northwind):
 # ---------- Delete ----------
 
 
+def test_only_owners_delete_and_they_must_type_the_name(client, alex, maya, northwind):
+    assert delete(client, alex, northwind, "Northwind Data Academy").status_code == 403
+    wrong = delete(client, maya, northwind, "northwind data academy")
+    assert wrong.status_code == 422 and "exactly" in wrong.json()["detail"]
+    assert client.get(f"/api/workspaces/{northwind}", headers=maya).status_code == 200
+
+
+def test_deleting_a_workspace_removes_its_content_and_tells_members(client, maya, northwind, db):
+    assert count(db, Course, Course.workspace_id == northwind) > 0
+    alex_id = db.scalars(select(User.id).where(User.email == "demo@learnloop.dev")).one()
+    assert delete(client, maya, northwind, "  Northwind Data Academy ").status_code == 204
+    db.expire_all()
+    assert db.get(Workspace, northwind) is None
+    for model in (Course, Membership, Invitation, Activity):
+        assert count(db, model, model.workspace_id == northwind) == 0, model.__name__
+    assert db.get(User, alex_id).last_workspace_id is None
+    note = db.scalars(select(Notification).where(Notification.user_id == alex_id)).all()[-1]
+    assert note.title == "Maya Chen deleted the workspace Northwind Data Academy"
+    alex = login(client, "demo@learnloop.dev")
+    assert [w["slug"] for w in client.get("/api/auth/me", headers=alex).json()["workspaces"]] == [
+        "biology-201-study-group"
+    ]
+    assert client.get(f"/api/workspaces/{northwind}", headers=alex).status_code == 404
+
+
+def test_deleting_a_workspace_keeps_other_workspaces_intact(client, alex, biology, northwind, db):
+    before = count(db, Course, Course.workspace_id == northwind)
+    assert delete(client, alex, biology, "Biology 201 Study Group").status_code == 204
+    assert count(db, Course, Course.workspace_id == northwind) == before
+    assert client.get(f"/api/workspaces/{northwind}/members", headers=alex).json()["total"] == 5
+
+
+# ---------- Leave ----------
+
+
+def test_member_leaves_a_workspace(client, sam, northwind, db):
+    response = client.post(f"/api/workspaces/{northwind}/leave", headers=sam)
+    assert response.status_code == 200
+    assert response.json()["workspaces"] == [] and response.json()["current_workspace_id"] is None
+    assert client.get(f"/api/workspaces/{northwind}", headers=sam).status_code == 404
+    assert count(db, Activity, Activity.verb == "member.left", Activity.workspace_id == northwind) == 1
+
+
+def test_last_owner_cannot_leave(client, maya, northwind):
+    response = client.post(f"/api/workspaces/{northwind}/leave", headers=maya)
+    assert response.status_code == 409
+    assert "only owner" in response.json()["detail"]
+
+
+def test_owner_can_leave_when_another_owner_remains(client, maya, alex, northwind, db):
+    alex_id = client.get("/api/auth/me", headers=alex).json()["user"]["id"]
+    client.patch(f"/api/workspaces/{northwind}/members/{alex_id}", headers=maya, json={"role": "owner"})
+    assert client.post(f"/api/workspaces/{northwind}/leave", headers=maya).status_code == 200
+    db.expire_all()
+    assert db.get(Workspace, northwind).owner_id == alex_id
+    assert client.get(f"/api/workspaces/{northwind}", headers=alex).json()["owner"]["name"] == "Alex Rivera"
