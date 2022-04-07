f62425c4bebbb8d907e9ec7dc891faf906f229c1
Paul <phaul.thompson@gmail.com>
2022-04-07 09:26:18 -0700

    Add ESLint 8 config with TypeScript and React hooks rules

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2022: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 2022, sourceType: 'module', ecmaFeatures: { jsx: true } },
+  plugins: ['@typescript-eslint', 'react-hooks'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
+  ignorePatterns: ['dist', 'node_modules'],
+  rules: {
+    '@typescript-eslint/no-non-null-assertion': 'off',
+  },
+};
