a4264432fed7f0730567af5391a2d76d43b83008
Paul <phaul.thompson@gmail.com>
2022-07-21 17:51:32 -0700

    Add task drawer

--- frontend/src/features/board/TaskDrawer.tsx
+++ frontend/src/features/board/TaskDrawer.tsx
@@ -147,3 +147,266 @@ function DescriptionEditor({ value, onSave }: { value: string; onSave: (descript
   );
 }
 
+function EstimateInput({ value, onSave }: { value: number | null; onSave: (estimate: number | null) => void }) {
+  const [draft, setDraft] = useState(value === null ? '' : String(value));
+  const error = validateEstimate(draft);
+  const commit = () => {
+    const next = draft.trim() ? Number(draft) : null;
+    if (!error && next !== value) onSave(next);
+  };
+  return (
+    <>
+      <input
+        className="input"
+        type="number"
+        inputMode="numeric"
+        min={0}
+        max={ESTIMATE_MAX}
+        value={draft}
+        aria-label="Estimate in points"
+        aria-invalid={error ? true : undefined}
+        placeholder="—"
+        onChange={(event) => setDraft(event.target.value)}
+        onBlur={commit}
+        onKeyDown={(event) => event.key === 'Enter' && commit()}
+      />
+      {error && <small className="field-error">{error}</small>}
+    </>
+  );
+}
+
+/** Everything about one task in a side drawer; addressable as /board?task=<number>. */
+export function TaskDrawer({ workspaceId, number, members, labels, courses, canManageLabels, me, onClose, onChanged, onDeleted, onLabelCreated }: TaskDrawerProps) {
+  const now = useNow();
+  const toast = useToast();
+  const loader = useLoader(() => boardApi.byNumber(workspaceId, number), `task:${workspaceId}:${number}`);
+  const [local, setLocal] = useState<{ source: TaskDetail | null; detail: TaskDetail | null }>({ source: null, detail: null });
+  if (loader.data && loader.data !== local.source) setLocal({ source: loader.data, detail: loader.data });
+  const detail = local.detail;
+  const [confirming, setConfirming] = useState(false);
+  // A nested confirmation (task or comment delete) owns Escape; the drawer must not close underneath it.
+  const [commentDialog, setCommentDialog] = useState(false);
+  const [deleting, setDeleting] = useState(false);
+  const lookups: PatchLookups = { members, courses, labels };
+
+  const commit = (next: TaskDetail) => {
+    setLocal((l) => ({ ...l, detail: next }));
+    onChanged(summarize(next));
+  };
+
+  const save = async (patch: TaskPatch) => {
+    if (!detail) return;
+    const previous = detail;
+    commit(applyPatch(detail, patch, lookups));
+    try {
+      commit(await boardApi.update(detail.id, patch));
+    } catch (err) {
+      commit(previous);
+      toast.error((err as Error).message);
+    }
+  };
+
+  const remove = async () => {
+    if (!detail) return;
+    setDeleting(true);
+    try {
+      await boardApi.remove(detail.id);
+      toast.success(`Deleted ${detail.key}`);
+      onDeleted(detail.id);
+    } catch (err) {
+      toast.error((err as Error).message);
+      setDeleting(false);
+      setConfirming(false);
+    }
+  };
+
+  const copyLink = () => {
+    const url = `${window.location.origin}/board?task=${number}`;
+    navigator.clipboard?.writeText(url).then(
+      () => toast.success('Link copied'),
+      () => toast.error('Could not copy the link'),
+    );
+  };
+
+  const title = detail ? (
+    <span className="drawer-heading">
+      <span className="task-key">{detail.key}</span>
+      <span className={`task-status-pill task-status-${detail.status}`}>{STATUS_LABELS[detail.status]}</span>
+    </span>
+  ) : (
+    'Task'
+  );
+
+  return (
+    <Modal
+      variant="drawer"
+      title={title}
+      onClose={confirming || commentDialog ? () => undefined : onClose}
+      footer={
+        detail && (
+          <>
+            <button type="button" className="ghost small" onClick={copyLink}>
+              <Link2 /> Copy link
+            </button>
+            <span className="spacer" />
+            {detail.can_delete && (
+              <button type="button" className="danger small" onClick={() => setConfirming(true)}>
+                <Trash2 /> Delete task
+              </button>
+            )}
+          </>
+        )
+      }
+    >
+      {!detail && loader.loading && <Loading label="Loading task…" />}
+      {!detail && loader.error && (
+        <EmptyState icon={<Search />} title={`Task #${number} is not available`}>
+          <p>{loader.error}. It may have been deleted, or it belongs to another workspace.</p>
+          <button type="button" className="secondary" onClick={loader.reload}>
+            Try again
+          </button>
+        </EmptyState>
+      )}
+      {detail && (
+        <div className="task-drawer">
+          <InlineTitle key={detail.title} value={detail.title} onSave={(value) => void save({ title: value })} />
+
+          <dl className="task-fields">
+            <div>
+              <dt>Status</dt>
+              <dd>
+                <select className="input" aria-label="Status" value={detail.status} onChange={(event) => void save({ status: event.target.value as TaskStatus })}>
+                  {STATUSES.map((status) => (
+                    <option key={status} value={status}>
+                      {STATUS_LABELS[status]}
+                    </option>
+                  ))}
+                </select>
+              </dd>
+            </div>
+            <div>
+              <dt>Priority</dt>
+              <dd>
+                <select className="input" aria-label="Priority" value={detail.priority} onChange={(event) => void save({ priority: event.target.value as TaskPriority })}>
+                  {PRIORITIES.map((priority) => (
+                    <option key={priority} value={priority}>
+                      {PRIORITY_LABELS[priority]}
+                    </option>
+                  ))}
+                </select>
+              </dd>
+            </div>
+            <div>
+              <dt>Assignee</dt>
+              <dd>
+                <select
+                  className="input"
+                  aria-label="Assignee"
+                  value={detail.assignee?.id ?? ''}
+                  onChange={(event) => void save({ assignee_id: event.target.value ? Number(event.target.value) : null })}
+                >
+                  <option value="">Unassigned</option>
+                  {members.map((member) => (
+                    <option key={member.id} value={member.id}>
+                      {member.id === me.id ? `${member.name} (me)` : member.name}
+                    </option>
+                  ))}
+                </select>
+              </dd>
+            </div>
+            <div>
+              <dt>Course</dt>
+              <dd>
+                <select
+                  className="input"
+                  aria-label="Course"
+                  value={detail.course?.id ?? ''}
+                  onChange={(event) => void save({ course_id: event.target.value ? Number(event.target.value) : null })}
+                >
+                  <option value="">No course</option>
+                  {detail.course && !courses.some((c) => c.id === detail.course!.id) && <option value={detail.course.id}>{detail.course.title}</option>}
+                  {courses.map((course) => (
+                    <option key={course.id} value={course.id}>
+                      {course.title}
+                    </option>
+                  ))}
+                </select>
+              </dd>
+            </div>
+            <div>
+              <dt>Due date</dt>
+              <dd className="due-field">
+                <input className="input" type="date" aria-label="Due date" value={detail.due_date ?? ''} onChange={(event) => void save({ due_date: event.target.value || null })} />
+                <DueBadge due={detail.due_date} status={detail.status} now={now} />
+              </dd>
+            </div>
+            <div>
+              <dt>Estimate</dt>
+              <dd>
+                <EstimateInput key={detail.estimate ?? 'none'} value={detail.estimate} onSave={(estimate) => void save({ estimate })} />
+              </dd>
+            </div>
+            <div className="wide-field">
+              <dt>Labels</dt>
+              <dd>
+                <LabelPicker
+                  workspaceId={workspaceId}
+                  labels={labels}
+                  selected={detail.labels}
+                  canCreate={canManageLabels}
+                  onChange={(label_ids) => void save({ label_ids })}
+                  onCreated={onLabelCreated}
+                />
+              </dd>
+            </div>
+          </dl>
+
+          <section className="drawer-section" aria-label="Description">
+            <h3>Description</h3>
+            <DescriptionEditor value={detail.description} onSave={(description) => void save({ description })} />
+          </section>
+
+          <ChecklistPanel taskId={detail.id} items={detail.checklist} onChange={(checklist) => commit({ ...detail, checklist })} />
+
+          <CommentThread
+            taskId={detail.id}
+            comments={detail.comments}
+            people={members}
+            me={me}
+            now={now}
+            onChange={(comments) => commit({ ...detail, comments })}
+            onConfirming={setCommentDialog}
+          />
+
+          <footer className="task-meta-footer">
+            <span>
+              Created by {detail.reporter?.name ?? 'a former member'} <time title={formatDateTime(detail.created_at)}>{relativeTime(detail.created_at, now)}</time>
+            </span>
+            <span>
+              Updated <time title={formatDateTime(detail.updated_at)}>{relativeTime(detail.updated_at, now)}</time>
+            </span>
+            {detail.completed_at && (
+              <span>
+                Completed <time title={formatDateTime(detail.completed_at)}>{relativeTime(detail.completed_at, now)}</time>
+              </span>
+            )}
+          </footer>
+        </div>
+      )}
+      {confirming && detail && (
+        <ConfirmDialog
+          title={`Delete ${detail.key}?`}
+          message={
+            <>
+              <b>{detail.title}</b> and its checklist and {detail.comments.length} comment{detail.comments.length === 1 ? '' : 's'} will be removed for everyone. This cannot be undone.
+            </>
+          }
+          confirmLabel="Delete task"
+          busy={deleting}
+          onConfirm={() => void remove()}
+          onCancel={() => setConfirming(false)}
+        />
+      )}
+    </Modal>
+  );
+}
